ba2bfc03cc3f07b549ee170dcf666673125e9617
Lenard Frommelt <lenny09918050@gmail.com>
2022-04-22 11:42:35 +0200

    Update copyscript.sh

--- copyscript.sh
+++ copyscript.sh
@@ -2,7 +2,6 @@
 ##
 cp sources/ressources/.gitignore output/.gitignore
 cp sources/ressources/LICENSE output/LICENSE
-cp sources/ressources/UNLICENSE output/UNLICENSE
 cp sources/ressources/package.json output/package.json
 cp sources/ressources/README.md output/README.md
 
